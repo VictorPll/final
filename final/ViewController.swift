791e74b5b4a50e270030c8c398ad1d77826e4048
VictorPll <46313672+VictorPll@users.noreply.github.com>
2019-01-05 19:03:54 -0600

    sadaS

--- final/ViewController.swift
+++ final/ViewController.swift
@@ -15,13 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-xcasdasdasdasdsdadasd
-    
-    
-    sadasdasdasdasdasdasd
-    
-    //asdasdasdasdasda
-    dasdda
+//Esto es un aprueba de validacion
     
     
 }
